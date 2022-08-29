df5651af7c1fd0c10d4479b64080cb95156d3691
11lugg <jlugg89@gmail.com>
2022-08-29 12:22:40 +0100

    removed console.log

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -27,7 +27,6 @@ export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
       userAuth,
       additionalDetails
     );
-    console.log(userSnapshot);
     yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
   } catch (error) {
     yield put(signInFailed(error));
